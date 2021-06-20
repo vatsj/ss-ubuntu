b71822bd886a487f12d11d8ee7090b18e2a354d9
Jacob Stavrianos <jstav@stanford.edu>
2021-06-20 18:06:44 -0400

    porting over old linux-setup script

---
+++ scripts/languages.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+printf "\n\n installing programming languages + accessory software \n"
+
+# conda
--- scripts/media.sh
+++ scripts/media.sh
@@ -1,3 +1,32 @@
 #!/bin/bash
 
 printf "\n\n installing media software \n"
+
+# photo editing
+ssi gimp
+sai inkscape
+
+# audio
+
+# linux audio drivers
+sai pulseaudio
+sai pavucontrol
+
+# simple audio player
+ssi vlc
+
+# yt-dl + ffmpeg
+sai youtube-dl
+sai ffmpeg
+
+# video editing - kdenlive
+ssi kdenlive
+
+
+# filetype readers
+
+# epub reader
+sai fbreader
+
+# simple PDF editing
+sai xournal
