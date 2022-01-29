53481c55bb35ff0b07357590a2d1b52d47c41c54
Jacob Stavrianos <jstav@stanford.edu>
2022-01-28 21:51:15 -0800

    added convex opt pkgs to Julia install

--- lang-scripts/julia_init.jl
+++ lang-scripts/julia_init.jl
@@ -4,7 +4,13 @@ using Pkg
 # kernel for jupyterlab
 Pkg.add("IJulia")
 
+# JSON parser
 Pkg.add("JSON")
 
+# linalg helpers (STN - ee263)
 Pkg.add("LinearAlgebra")
 Pkg.add("Plots")
+
+## convex opt (STN - ee364a)
+Pkg.add("Convex")
+Pkg.add("SCS")
